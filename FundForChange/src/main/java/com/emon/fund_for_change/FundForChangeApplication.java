d83483f6a6fc569aac4878004223177c8b13a690
emon253 <emonchowdhury253@gmail.com>
2022-02-10 11:24:30 +0600

    First Commit

---
+++ FundForChange/src/main/java/com/emon/fund_for_change/FundForChangeApplication.java
@@ -0,0 +1,13 @@
+package com.emon.fund_for_change;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundForChangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundForChangeApplication.class, args);
+	}
+
+}
---
+++ FundForChange/src/test/java/com/emon/fund_for_change/FundForChangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emon.fund_for_change;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundForChangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
